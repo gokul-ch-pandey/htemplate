43c940f41e8948db306fe18e215bf33e61a0e4bf
Gokul <gokul.pandey@nagarro.com>
2019-05-24 17:14:05 +0530

    Template : spring + angular + oauth

---
+++ htemplate/src/main/java/com/hikex/htemplate/HtemplateApplication.java
@@ -0,0 +1,13 @@
+package com.hikex.htemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtemplateApplication.class, args);
+	}
+
+}
---
+++ htemplate/src/test/java/com/hikex/htemplate/HtemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hikex.htemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

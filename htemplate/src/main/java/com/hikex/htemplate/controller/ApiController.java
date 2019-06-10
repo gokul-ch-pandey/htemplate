6307b973c1b99073219caa1b1a3d75eb1a476096
Akash Shrivastava <akash.shrivastava@nagarro.com>
2019-06-10 12:33:12 +0530

    Swagger_Implementation_initial_1

--- htemplate/src/main/java/com/hikex/htemplate/controller/ApiController.java
+++ htemplate/src/main/java/com/hikex/htemplate/controller/ApiController.java
@@ -1,15 +1,23 @@
 package com.hikex.htemplate.controller;
 
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @RestController
 @RequestMapping(value = "/api")
+@Api(value="HTemplate SWAGGER API")
 public class ApiController {
 
+	@ApiOperation(value="API for hello")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Success")})
 	@RequestMapping(value = "/hello")
 	public String helloWorld() {
 		return "hello world";
 	}
-
 }

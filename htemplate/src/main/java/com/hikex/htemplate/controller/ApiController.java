1cfbdfc1dd3809e7b07b7b3000772a74720eea01
Akash Shrivastava <akash.shrivastava@nagarro.com>
2019-06-10 13:03:21 +0530

    Swagger configuration chnages

--- htemplate/src/main/java/com/hikex/htemplate/controller/ApiController.java
+++ htemplate/src/main/java/com/hikex/htemplate/controller/ApiController.java
@@ -1,20 +1,29 @@
 package com.hikex.htemplate.controller;
 
+
 import java.security.Principal;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 
 import com.hikex.htemplate.dto.UserData;
 
+
 @RestController
 @RequestMapping(value = "/api")
-public class ApiController extends BaseController {
+@Api(value="HTemplate SWAGGER API")
+public class ApiController extends BaseController{
 
-	@RequestMapping(value = "/hello", produces = "application/json")
+	@ApiOperation(value="API for hello")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Success")})
+	@RequestMapping(value = "/hello")
+		@RequestMapping(value = "/hello", produces = "application/json")
 	public UserData helloWorld(Principal user) {
-
-		return dataMapper.map(user, UserData.class);
+	return dataMapper.map(user, UserData.class);
 	}
-
 }
--- htemplate/src/main/java/com/hikex/htemplate/security/WebSecurity.java
+++ htemplate/src/main/java/com/hikex/htemplate/security/WebSecurity.java
@@ -3,21 +3,29 @@ package com.hikex.htemplate.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
-	
+
 	@Bean
 	@Override
 	protected AuthenticationManager authenticationManager() throws Exception {
 		return super.authenticationManager();
 	}
-	
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("admin").password("{noop}nimda").roles("USER");
+		auth.inMemoryAuthentication().withUser("admin").password("{noop}nimda").roles("USER").and().withUser("akash")
+				.password("{noop}abcd123").roles("CUSTOMER");
+	}
+	
+	@Override
+	public void configure(org.springframework.security.config.annotation.web.builders.WebSecurity web)
+			throws Exception {
+		web.ignoring().antMatchers("/swagger-ui.html");
 	}
 
 }
